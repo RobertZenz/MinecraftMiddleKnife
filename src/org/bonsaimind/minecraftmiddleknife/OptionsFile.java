157df7191a0b0b53f53759bc9a55429483cbe01f
Robert Zenz <bobby@bonsaimind.org>
2013-08-31 19:05:45 +0200

    Removed unnecessary import.

--- src/org/bonsaimind/minecraftmiddleknife/OptionsFile.java
+++ src/org/bonsaimind/minecraftmiddleknife/OptionsFile.java
@@ -34,7 +34,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 
 /**
@@ -148,7 +147,7 @@ public class OptionsFile {
 
 	/**
 	 * Set the path to the options.txt file.
-	 * @param pathOrFile 
+	 * @param pathOrFile
 	 */
 	public void setPath(String pathOrFile) {
 		this.file = new File(pathOrFile);
