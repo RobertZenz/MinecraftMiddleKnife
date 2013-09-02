ef99a24b4c2bb2ba3b775894e27bcff518d0015c
Robert Zenz <Bobby@bonsaimind.org>
2013-09-02 22:59:09 +0200

    Extended with some fields and parsing of the answers.

--- src/org/bonsaimind/minecraftmiddleknife/Yggdrasil.java
+++ src/org/bonsaimind/minecraftmiddleknife/Yggdrasil.java
@@ -29,9 +29,11 @@ package org.bonsaimind.minecraftmiddleknife;
 
 import java.util.UUID;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
- * Deals with the new authentication function called...Yggdrasil.
+ * Deals with the new authentication system called...Yggdrasil.
  */
 public class Yggdrasil extends Credentials {
 
@@ -41,9 +43,12 @@ public class Yggdrasil extends Credentials {
 	public static final String ENDPOINT_REFRESH = "refresh";
 	public static final String ENDPOINT_VALIDATE = "validate";
 	public static final String MOJANG_SERVER = "https://authserver.mojang.com/";
+	private String accessToken;
 	private String agentName = AGENT_NAME;
 	private int agentVersion = AGENT_VERSION;
 	private String clientToken = UUID.randomUUID().toString();
+	private String realUsername;
+	private String userId;
 
 	public Yggdrasil() {
 	}
@@ -54,6 +59,10 @@ public class Yggdrasil extends Credentials {
 
 	public Yggdrasil(String username, String password) {
 		super(username, password);
+	}
+
+	public String getAccessToken() {
+		return accessToken;
 	}
 
 	public String getAgentName() {
@@ -68,6 +77,18 @@ public class Yggdrasil extends Credentials {
 		return clientToken;
 	}
 
+	public String getRealUsername() {
+		return realUsername;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
 	public void setAgentName(String agentName) {
 		this.agentName = agentName;
 	}
@@ -78,6 +99,14 @@ public class Yggdrasil extends Credentials {
 
 	public void setClientToken(String clientToken) {
 		this.clientToken = clientToken;
+	}
+
+	public void setRealUsername(String realUsername) {
+		this.realUsername = realUsername;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
 	private String createAuthenticationJSON() {
@@ -98,22 +127,46 @@ public class Yggdrasil extends Credentials {
 	private String createRefreshJSON() {
 		JSONObject parent = new JSONObject();
 
-		parent.put("accessToken", "ACCESSTOKEN");
+		parent.put("accessToken", getAccessToken());
 		parent.put("clientToken", getClientToken());
 
 		JSONObject selectedProfile = new JSONObject();
-		selectedProfile.put("id", "ID");
+		selectedProfile.put("id", getUserId());
 		selectedProfile.put("name", getUsername());
 		parent.put("selectedProfile", selectedProfile);
 
 		return parent.toJSONString();
 	}
-	
+
 	private String createValidationJSON() {
 		JSONObject parent = new JSONObject();
-		
-		parent.put("accessToken", "ACCESSTOKEN");
-		
+
+		parent.put("accessToken", getAccessToken());
+
 		return parent.toJSONString();
+	}
+
+	private void parseAuthenticationJSON(String response) throws ParseException {
+		JSONParser parser = new JSONParser();
+		JSONObject parent = (JSONObject) parser.parse(response);
+		setAccessToken((String) parent.get("accessToken"));
+
+		JSONObject selectedProfile = (JSONObject) parent.get("selectedProfile");
+		setUserId((String) selectedProfile.get("id"));
+		setRealUsername((String) selectedProfile.get("name"));
+	}
+
+	private void parseErrorJSON(String response) throws ParseException {
+		// Stand tall for the man next door!
+	}
+	
+	private void parseRefreshJSON(String response) throws ParseException {
+		JSONParser parser = new JSONParser();
+		JSONObject parent = (JSONObject) parser.parse(response);
+		setAccessToken((String) parent.get("accessToken"));
+
+		JSONObject selectedProfile = (JSONObject) parent.get("selectedProfile");
+		setUserId((String) selectedProfile.get("id"));
+		setRealUsername((String) selectedProfile.get("name"));
 	}
 }
