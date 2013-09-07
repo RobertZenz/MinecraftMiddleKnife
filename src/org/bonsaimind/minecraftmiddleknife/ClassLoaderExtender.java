078decc7f5f9852641c2c18655036731e690c241
Robert Zenz <bobby@bonsaimind.org>
2013-09-07 18:40:22 +0200

    Added CLassLoaderExtender...and I think I fixed that shit I did with the Loader of the current thread.

---
+++ src/org/bonsaimind/minecraftmiddleknife/ClassLoaderExtender.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2012 Robert 'Bobby' Zenz. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Robert 'Bobby' Zenz ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Robert 'Bobby' Zenz.
+ */
+package org.bonsaimind.minecraftmiddleknife;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * A static helper that let's you extend the System- and Thread-Classloader.
+ */
+public final class ClassLoaderExtender {
+
+	/**
+	 * Adds the given URLs to the classloeaders.
+	 * @param urls
+	 * @throws NoSuchMethodException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
+	public static void extend(URL[] urls) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		// Extend the ClassLoader of the current thread.
+		URLClassLoader loader = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
+
+		// Extend the SystemClassLoader...this is needed for mods which will
+		// use the WhatEver.getClass().getClassLoader() method to retrieve
+		// a ClassLoader.
+		URLClassLoader systemLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+
+		// Get the method via reflection.
+		Method addURLMethod = URLClassLoader.class.getDeclaredMethod("addURL", new Class[]{URL.class});
+		addURLMethod.setAccessible(true);
+
+		for (URL url : urls) {
+			addURLMethod.invoke(systemLoader, url);
+			addURLMethod.invoke(loader, url);
+		}
+	}
+}
