95a86e08263f07b4b251c7383b740063e3d7e52a
Robert Zenz <Bobby@bonsaimind.org>
2013-10-29 22:41:06 +0100

    Added package info files.

---
+++ src/org/bonsaimind/minecraftmiddleknife/post16/package-info.java
@@ -0,0 +1,4 @@
+/*
+ * Contains classes to versions after the 1.6 update.
+ */
+package org.bonsaimind.minecraftmiddleknife.post16;
---
+++ src/org/bonsaimind/minecraftmiddleknife/pre16/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains classes to handle versions before the 1.6 update.
+ */
+package org.bonsaimind.minecraftmiddleknife.pre16;
