246ab0793eca84837f45d7ac25f6493e8f59c54c
Robert Zenz <bobby@bonsaimind.org>
2013-12-29 14:00:52 +0100

    Added private constructor.

--- src/org/bonsaimind/minecraftmiddleknife/pre16/Authenticator.java
+++ src/org/bonsaimind/minecraftmiddleknife/pre16/Authenticator.java
@@ -43,6 +43,9 @@ import org.bonsaimind.minecraftmiddleknife.Credentials;
  */
 public final class Authenticator {
 
+	private Authenticator() {
+		throw new AssertionError(); // Not supposed to instance this.
+	}
 	/**
 	 * The default version which will be reported.
 	 */
