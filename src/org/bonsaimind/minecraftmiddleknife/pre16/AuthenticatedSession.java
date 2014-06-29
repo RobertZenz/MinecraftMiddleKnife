02626004af74907554260f5310d786d3fb74e151
Robert Zenz <bobby@bonsaimind.org>
2014-06-29 22:10:00 +0200

    Moved static function to the end.

--- src/org/bonsaimind/minecraftmiddleknife/pre16/AuthenticatedSession.java
+++ src/org/bonsaimind/minecraftmiddleknife/pre16/AuthenticatedSession.java
@@ -54,28 +54,6 @@ public final class AuthenticatedSession {
 	}
 	
 	/**
-	 * Creates an AuthenticatedSession from the string representation.
-	 * 
-	 * @param value Needs to be in the format
-	 *            "currentVersion:downloadTicket:username:sessionId:userId".
-	 * @return The AuthenticatedSession for this string representation.
-	 */
-	public static AuthenticatedSession fromString(String value) {
-		if (value == null) {
-			throw new IllegalArgumentException("value is not allowed to be null.");
-		}
-		
-		String[] splittedValue = value.split(":");
-		
-		if (splittedValue.length != 5) {
-			throw new IllegalArgumentException("value is not in the expected format \"currentVersion:downloadTicket:username:sessionId:userId\" but was \""
-					+ value + "\".");
-		}
-		
-		return new AuthenticatedSession(Long.parseLong(splittedValue[0]), splittedValue[1], splittedValue[2], splittedValue[3], splittedValue[4]);
-	}
-	
-	/**
 	 * Returns the UNIX-timestamp of the current version of Minecraft.
 	 * 
 	 * @return The current version as UNIX-timestamp.
@@ -131,5 +109,27 @@ public final class AuthenticatedSession {
 	@Override
 	public String toString() {
 		return String.format("%s:%s:%s:%s:%s", currentVersion, downloadTicket, username, sessionId, userId);
+	}
+	
+	/**
+	 * Creates an AuthenticatedSession from the string representation.
+	 * 
+	 * @param value Needs to be in the format
+	 *            "currentVersion:downloadTicket:username:sessionId:userId".
+	 * @return The AuthenticatedSession for this string representation.
+	 */
+	public static AuthenticatedSession fromString(String value) {
+		if (value == null) {
+			throw new IllegalArgumentException("value is not allowed to be null.");
+		}
+		
+		String[] splittedValue = value.split(":");
+		
+		if (splittedValue.length != 5) {
+			throw new IllegalArgumentException("value is not in the expected format \"currentVersion:downloadTicket:username:sessionId:userId\" but was \""
+					+ value + "\".");
+		}
+		
+		return new AuthenticatedSession(Long.parseLong(splittedValue[0]), splittedValue[1], splittedValue[2], splittedValue[3], splittedValue[4]);
 	}
 }
